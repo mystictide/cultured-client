d77ad2d6e4e9265efeee8b40fbb7e629de71a284
Furkan Güler <furkang2k20@outlook.com>
2023-04-27 19:50:39 +0300

    vite base fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: 'https://cultured.herrguller.cc/',
   plugins: [react()],
 })
